f142bc26b3b97d8128ff36fa7d413a7897b53351
Joon Park <sargadil@gmail.com>
2023-04-24 16:42:14 +0900

    Joon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.js'
-// import App from './Class/06/redux/App';
-// import App from './nico/practice.js';
-// import App from './practice/MemoriesReminder/App.js';
-// import App from './card';
+// import App from './App.js'
+import App from './pages/reference.jsx';
 
 
 const container = document.getElementById("root");
