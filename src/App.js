f3e9dc13a6064a4f346efa8a48370d0bed4b3c2b
FeliceKP <qpalzm192837@naver.com>
2023-04-25 15:41:51 +0900

    강의폴더생성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./App.css";
+import "./styles/mainpage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import Banner from "./pages/Banner.js";
+import Reference from "./pages/reference.js";
+import Pages from "./pages/pages.js";
 
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import './App.css';
-import './styles/mainpage.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Footer from './components/Footer';
-import Banner from './pages/Banner.js'
-import Reference from './pages/reference.js'
-import Pages from './pages/pages.js'
-
-import { Routes, Route, Link, useNavigate } from "react-router-dom"
-
-
-
-
-
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function App() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    return (
-        <div className='App'>
-            <Navbar bg='light' variant='light'>
-                <Container>
-                    <Navbar.Brand href="/">npm</Navbar.Brand>
-                    <Nav>
-                        <Nav.Link onClick={() => { navigate('/class') }} id="NavSet">강의</Nav.Link>
-                        <Nav.Link onClick={() => { navigate('/reference') }} id="NavSet">레퍼런스</Nav.Link>
-                        <Nav.Link onClick={() => { navigate('/login') }} id="NavSet">로그인</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/">npm</Navbar.Brand>
+          <Nav>
+            <Nav.Link
+              onClick={() => {
+                navigate("/class");
+              }}
+              id="NavSet"
+            >
+              강의
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/reference");
+              }}
+              id="NavSet"
+            >
+              레퍼런스
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/login");
+              }}
+              id="NavSet"
+            >
+              로그인
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-            <Routes>
-            <Route path='/' element={  <Banner />} />
-                <Route path='/class' element={<Pages />} />
-                <Route path='/reference' element={<Reference />} />
-                <Route path='/login' element={<></>} />
-            </Routes>
-            <Footer />
-        </div>
-    )
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/class" element={<Pages />} />
+        <Route path="/reference" element={<Reference />} />
+        <Route path="/login" element={<></>} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 export default App;
