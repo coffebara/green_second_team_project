33589f372010273d8737904365b0c798293da5f3
jayji5204 <jayji5204@naver.com>
2023-04-25 11:05:06 +0900

    데뷔1일차

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import {Button,Navbar,Container,Nav} from 'react-bootstrap'
+import {Navbar,Container,Nav} from 'react-bootstrap'
 import './App.css';
 import './styles/mainpage.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Banner from './components/Banner.js'
+import Banner from './pages/Banner.js'
 import Footer from './components/Footer';
+import Reference from './pages/reference'
 import { Routes, Route, useNavigate} from "react-router-dom"
 
 
@@ -27,14 +28,16 @@ function App(){
                     </Nav>
                 </Container>
             </Navbar>
-            <Container>
+            
                 <Banner/>
-                <Footer/>
-            </Container>
+                <Container>
+                    <Footer/>
+                    </Container>
+           
             <Routes>
                 {/* <Route path='/class' element={<Class/>}>
                 <Route path='/reference' element={<Reference/>}> */}
-                <Route path='/' element={<></>}>
+                <Route path='/reference' element={<Reference/>}>
                 </Route>
               
             </Routes> 
