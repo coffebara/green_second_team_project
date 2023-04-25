40fa2978df414561240ea932c487b2f7137f323a
allready-dawn <qwshpke@naver.com>
2023-04-25 11:08:18 +0900

    cart 페이지 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.js'
+// import App from './App.js'
 import { BrowserRouter } from "react-router-dom"
 // import App from './Class/06/redux/App';
 // import App from './nico/practice.js';
 // import App from './practice/MemoriesReminder/App.js';
 // import App from './card';
+import App from './pages/cart'
 
 
 const container = document.getElementById("root");
---
+++ src/pages/cart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../styles/cart.css'
+
+function Cart() {
+    return (
+        <div>
+            <nav id="cart_tmpNav">임시 nav입니다</nav>
+            <section id="cart_title">
+                <div className="cart_container">배너 타이틀 신청하기</div>
+            </section>
+            <section id="cart_content">
+                <div className="cart_container cart_grid">
+                    <div className="cart_list">강의 신청 리스트</div>
+                    <div className="cart_total">강의 토탈 장바구니zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</div>
+                </div>
+            </section>
+            <footer id="cart_tmpFooter"></footer>
+        </div>
+    );
+}
+
+export default Cart;
