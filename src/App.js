4cbcf28cd348b2fa1a8b58dd1c6cfd744ee9c52b
FeliceKP <qpalzm192837@naver.com>
2023-04-25 15:48:18 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -46,28 +46,15 @@ function App() {
         </Container>
       </Navbar>
 
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Banner />} />
         <Route path="/class" element={<Pages />} />
         <Route path="/reference" element={<Reference />} />
         <Route path="/login" element={<></>} />
       </Routes>
+
       <Footer />
     </div>
   );
-=======
-            <Routes>
-            <Route path='/' element={  <Banner />} />
-                <Route path='/class' element={<Pages />} />
-                <Route path='/reference' element={<Reference />} />
-                <Route path='/login' element={<></>} />
-            </Routes>
-
-            
-            <Footer />
-        </div>
-    )
->>>>>>> 573f75a0ca15f8bcbccf3da5e440c0108b0a3bcf
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import "./index.css";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-// import App from "./lectureApp";
+import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
-// import App from './Class/06/redux/App';
-// import App from './nico/practice.js';
-// import App from './practice/MemoriesReminder/App.js';
-// import App from './card';
+import { Provider } from "react-redux";
+import store from "./store";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-=======
-import React from 'react';
-import './index.css';
-import { createRoot } from 'react-dom/client';
-import App from './App.js';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import store from './store';
-
-const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
@@ -35,6 +15,5 @@ root.render(
         <App />
       </BrowserRouter>
     </Provider>
->>>>>>> 573f75a0ca15f8bcbccf3da5e440c0108b0a3bcf
   </React.StrictMode>
 );
