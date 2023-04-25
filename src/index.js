4eba8b34d4e5569a12d6d96fdcb89b850b0833ed
Joon Park <sargadil@gmail.com>
2023-04-25 15:15:39 +0900

    ready2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './index.css';
 import { createRoot } from 'react-dom/client';
-import App from './App.js'
-import { BrowserRouter } from "react-router-dom"
+import App from './App.js';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store';
 
-
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-    <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-)
+);
