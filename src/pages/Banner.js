06795d46d11fadf0daf54f6f6e8110476da1e86c
jayji5204 <jayji5204@naver.com>
2023-04-27 16:53:17 +0900

    sfsf

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -6,6 +6,7 @@ import Footer_Dark from "./Footer_Dark";
 import { useNavigate } from "react-router-dom"
 
 
+
 export default function Banner(){
     let navigate = useNavigate();
     return(
@@ -16,7 +17,7 @@ export default function Banner(){
                 Learn to code</h1>
                <div id="Banner_bg1_center2">코딩을 진짜로 만들어볼까요?<br/>
                 실제 서비스를 따라 만들면서 코딩을 배우세요</div>
-                <Button id="Banner_bg1_button" variant="outline-dark" onClick={()=>{navigate('/class')}} >시작하기 →</Button>
+                <Button id="Banner_bg1_button" variant="outline-dark" onClick={()=>{navigate('/class')}} >시작하기→</Button>
                 </div>
 
 
