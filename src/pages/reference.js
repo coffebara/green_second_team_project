cbdb9bf87949632689cbd5d19e42e7ff54289f38
Joon Park <sargadil@gmail.com>
2023-04-27 15:47:07 +0900

    reference

--- src/pages/reference.js
+++ src/pages/reference.js
@@ -3,13 +3,13 @@ import '../styles/Nav.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav_Dark from "./Nav_Dark.js";
 import ReferenceList from '../components/Reperence/Lists';
-import ReferenceContents from '../components/Reperence/Contents';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Reperence/Home';
 import React01 from '../components/Reperence/React01';
 import React02 from '../components/Reperence/React02';
 import Js01 from '../components/Reperence/Js01';
 import Js02 from '../components/Reperence/Js02';
+import Js03 from '../components/Reperence/Js03';
 
 function Reference() {
   return (
@@ -23,6 +23,7 @@ function Reference() {
           <Route path="/react02" element={<React02 />} />
           <Route path="/js01" element={<Js01 />} />
           <Route path="/js02" element={<Js02 />} />
+          <Route path="/js03" element={<Js03 />} />
         </Routes>
       </div>
     </div>
