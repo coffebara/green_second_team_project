8fbbbcbceb8f8d1ee81fe27962b25e0fdb75d44a
allready-dawn <qwshpke@naver.com>
2023-04-28 09:56:07 +0900

    카트 & 결제페이지 업데이트 및 네비 경로수정

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,114 +1,109 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"
 import "../styles/cart.css";
 import data from "../assets/data.js";
-import Nav_Dark from "../pages/Nav_Dark.js";
-import Nav_Light from "../pages/Nav_Light.js";
-import Footer_Dark from "../pages/Footer_Dark.js";
+import Nav_Dark from "../components/Nav_Dark.js";
+import Nav_Light from "../components/Nav_Light.js";
+import Footer from "../components/Footer_Dark"
 
 // data.js에서 받은 값으로 장바구니 리스트 뿌리기
 const CartList = () => {
-  const [course, setCourse] = useState(data);
-  // 변경된 수량을 반영하는 함수
-  const handleChange = (idx, quantity) => {
-    setCourse((current) => {
-      const newCourse = [...current];
-
-      // 수량이 1밑으로 안떨어지게 하는 조건문
-      quantity < 1 ? (quantity = 1) : (newCourse[idx].quantity = quantity);
-      return newCourse;
+    const [course, setCourse] = useState(data);
+    // 변경된 수량을 반영하는 함수
+    const handleChange = (idx, quantity) => {
+        setCourse(current => {
+            const newCourse = [...current]
+            // 수량이 1밑으로 안떨어지게 하는 조건문
+            quantity < 1 ? quantity = 1 : newCourse[idx].quantity = quantity
+            return newCourse;
+        })
+    };
+    // data.js 에서 장바구니 리스트 출력
+    return course.map((item, idx) => {
+        return (
+            <tr key={item.id}>
+                <td><span className="cart_cancelBtn">X</span></td>
+                <td>{item.title}</td>
+                <td className="cart_price">{(item.price).toLocaleString()}₩</td>
+                <td>
+                    <input className="cart_input" type="number" id="a" onChange={(e)=>handleChange(idx, e.target.value)} value={item.quantity} />
+                </td>
+                <td className="cart_price">{(item.price * item.quantity).toLocaleString()}₩</td>
+            </tr>
+        );
     });
-  };
-  return course.map((item, idx) => {
-    return (
-      <tr key={item.id}>
-        <td>X</td>
-        <td>{item.title}</td>
-        <td>{item.price}</td>
-        <td>
-          <input
-            type="number"
-            id="a"
-            onChange={(e) => handleChange(idx, e.target.value)}
-            value={item.quantity}
-          />
-        </td>
-        <td>{item.price * item.quantity}</td>
-      </tr>
-    );
-  });
 };
 
 function Cart() {
-  return (
-    <div>
-      <Nav_Dark />
-      <section id="cart_title">
-        <div className="cart_container">
-          신청하기
-          <br />
-          Step 1. 강의 선택
-        </div>
-      </section>
-      <section id="cart_content">
-        <div className="cart_container cart_gridContainer">
-          <div className="cart_list">
-            <form method="post">
-              <table className="cart_table">
-                <thead id="cart_thead">
-                  <tr>
-                    <th>&nbsp;</th>
-                    <th>강좌</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* <tr>
+    let navigate = useNavigate();
+    return (
+        <div>
+            <Nav_Dark/>
+            <section id="cart_title">
+                <div className="cart_container">신청하기<br/>Step 1. 강의 선택</div>
+            </section>
+            <section id="cart_content">
+                <div className="cart_container cart_gridContainer">
+                    <div className="cart_list">
+                        <form method="post">
+                            <table className="cart_table">
+                                <thead id="cart_thead">
+                                    <tr>
+                                        <th>&nbsp;</th>
+                                        <th>강좌</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <CartList />
+                                    {/* CartList 출력 예시 
+                                    <tr>
                                         <td>X</td>
                                         <td>Next.js로 웹서비스 만들기</td>
                                         <td>35,000\</td>
                                         <td>1</td>
                                         <td>35,000</td>
                                     </tr> */}
-                  <CartList />
-                  <tr>
-                    <td colSpan="5">
-                      <div>
-                        <input type="text" placeholder="쿠폰 코드" />
-                        <input type="submit" value="쿠폰 적용하기" />
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </form>
-          </div>
-          <div className="cart_total">
-            <div>
-              <h4>신청할 강좌 합계</h4>
-            </div>
-            <table className="cart_table total_table">
-              <tbody>
-                <tr>
-                  <td>소계</td>
-                  <td className="total_price">1,200,000</td>
-                </tr>
-                <tr>
-                  <td>총계</td>
-                  <td className="total_price">1,200,000</td>
-                </tr>
-              </tbody>
-            </table>
-            <div>
-              <input type="button" value="결제 진행하기" />
-            </div>
-          </div>
+                                    <tr>
+                                        <td colSpan="5">
+                                            <div>
+                                                <input className="cart_coupon" type="text" placeholder="쿠폰 코드" />
+                                                <input id="cart_coupon_btn" type="submit" value="쿠폰 적용하기" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </form>
+                    </div>
+                    <div className="cart_total">
+                        <div>
+                            <h4>신청할 강좌 합계</h4>
+                        </div>
+                        <table className="cart_table total_table">
+                            <tbody>
+                                <tr>
+                                    <td>소계</td>
+                                    <td className="total_price">284,000₩</td>
+                                </tr>
+                                <tr>
+                                    <td>총계</td>
+                                    <td className="total_price">284,000₩</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div>
+                          {/* 결제페이지 이동 */}
+                            <input onClick={() => { navigate('/checkout') }} id="cart_process_btn" type="button" value="결제 진행하기" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer/>
         </div>
-      </section>
-      <Footer_Dark />
-    </div>
-  );
+    );
 }
 
 export default Cart;
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,64 +1,164 @@
 import React, { useState } from "react";
 import "../styles/checkout.css";
-import data from '../assets/data.js'
-import Nav_Dark from "../pages/Nav_Dark.js";
-import Nav_Light from "../pages/Nav_Light.js";
-import Footer_Light from "../pages/Footer_Light";
-
-
+import data from "../assets/data.js";
+import Nav_Dark from "../components/Nav_Dark.js";
+import Nav_Light from "../components/Nav_Light.js";
+import Footer from "../components/Footer_Dark.js";
 
 function Checkout() {
-    const [title, setTitle] = useState(data)
-    console.log(title)
+    const [title, setTitle] = useState(data);
+    console.log(title);
     return (
         <div>
-            <Nav_Dark/>
+            <Nav_Dark />
             <section id="checkout_title">
-                <div className="checkout_container">배너 타이틀 결제하기</div>
+                <div className="checkout_container">
+                    신청하기
+                    <br />
+                    Step 2. 결제
+                </div>
             </section>
             <section id="checkout_content">
-                <div className="checkout_container">
+                <div className="checkout_container checkout_contents">
                     <div>
                         <p>* 무통장입금 결제시 입금확인까지 평일 영업시간 내 10~20분 소요됩니다.</p>
-                        <p>* 영수증/수강증은 본인 이메일로 발송됩니다.</p>
+                        <p>
+                            * <strong>영수증/수강증</strong>은 본인 이메일로 발송됩니다.
+                        </p>
                     </div>
                     <div id="checkout_info">
-                        <div>
-                            <h3>신청자 정보</h3>
-                            <ul>
-                                <li>이름</li>
-                                <li>
-                                    <input type="text" placeholder="김상준" />
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>이메일</li>
-                                <li>
-                                    <input type="email" placeholder="rlatkdwns@naver.com" />
-                                </li>
-                            </ul>
+                        <div className="checkout_info_title">
+                            <h3 className="checkout_info_subtitle">신청자 정보</h3>
+                            <div className="checkout_info_contents">
+                                <ul>
+                                    <li>이름</li>
+                                    <li>
+                                        <input className="checkout_input" type="text" placeholder="김상준" />
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li>이메일</li>
+                                    <li>
+                                        <input className="checkout_input" type="email" placeholder="rlatkdwns@naver.com" />
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div>
-                            <h3>추가 정보</h3>
-                            <ul>
-                                <li>휴대폰번호</li>
-                                <li>
-                                    <input type="tel" placeholder="" />
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>기타사항 (선택 사항)</li>
-                                <li>
-                                    <textarea name="" id="" cols="100" rows="3" placeholder=" (무통장입금시 이체자 성함 등)"></textarea>
-                                </li>
-                            </ul>
+                        <div className="checkout_info_title">
+                            <h3 className="checkout_info_subtitle">추가 정보</h3>
+                            <div className="checkout_info_contents">
+                                <ul>
+                                    <li>휴대폰번호</li>
+                                    <li>
+                                        <input className="checkout_input" type="tel" placeholder="" />
+                                    </li>
+                                </ul>
+                                <ul>
+                                    <li>기타사항 (선택 사항)</li>
+                                    <li>
+                                        <textarea name="" className="checkout_area" id="" cols="100" rows="2" placeholder=" (무통장입금시 이체자 성함 등)"></textarea>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="checkout_info_title">
+                            <h3 className="checkout_info_subtitle">신청한 강좌</h3>
+                            <div className="checkout_info_contents">
+                                <table className="checkout_table">
+                                    <thead>
+                                        <tr>
+                                            <th>강좌</th>
+                                            <th>소계</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>HTML/CSS × 1</td>
+                                            <td>
+                                                <span className="checkout_price">75,000₩</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>JavaScript 입문 × 1</td>
+                                            <td>
+                                                <span className="checkout_price">78,000₩</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>JavaScript ES6 × 1</td>
+                                            <td>
+                                                <span className="checkout_price">56,000₩</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>React × 1</td>
+                                            <td>
+                                                <span className="checkout_price">75,000₩</span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <td>
+                                                <strong>소계</strong>
+                                            </td>
+                                            <td>
+                                                <span className="checkout_price">284,000₩</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <strong>총계</strong>
+                                            </td>
+                                            <td>
+                                                <span className="checkout_price">284,000₩</span>
+                                            </td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                                <div id="payment_box">
+                                    <div className="payment_method">
+                                        <ul>
+                                            <li>
+                                                <input type="radio" name="checkout_payment" id="checkout_atm" />
+                                                <label for="checkout_atm">무통장입금</label>
+                                            </li>
+                                            <li>
+                                                <input type="radio" name="checkout_payment" id="checkout_acountAtm" />
+                                                <label for="checkout_acountAtm">무통장입금 (가상계좌생성)</label>
+                                            </li>
+                                            <li>
+                                                <input type="radio" name="checkout_payment" id="checkout_credit" />
+                                                <label for="checkout_credit">신용카드 결제</label>
+                                            </li>
+                                            <li>
+                                                <input type="radio" name="checkout_payment" id="checkout_rtAcount" />
+                                                <label for="checkout_rtAcount">실시간계좌 이체</label>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="payment_agreement">
+                                        <div><p>입력하신 개인정보는 사이트내에서 이용할 예정이며 <span className="payment_agreement_span">개인정보 보호정책</span>을(를) 준수하고 있습니다.</p></div>
+                                        <p id="payment_mustCheck"><input type="checkbox" />
+                                        본인은 웹사이트 <span className="payment_agreement_span">이용 약관</span>을(를) 읽었으며 이에 동의합니다. </p> 
+                                    <input id="payment_btn" type="button" value="결제하기"/>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <Footer_Light/>
+            <Footer />
         </div>
     );
 }
+
+<table>
+    <tr>
+        <td>1번</td>
+        <td>2번</td>
+    </tr>
+</table>;
 
 export default Checkout;
