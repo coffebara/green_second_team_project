4b96858e086696b6624404f81f78dd94d83b88ff
FeliceKP <qpalzm192837@naver.com>
2023-05-02 12:01:43 +0900

    dsf

--- src/pages/lecture.js
+++ src/pages/lecture.js
@@ -12,70 +12,74 @@ function Lecture() {
   let [products] = useState(data);
   let navigate = useNavigate();
   return (
-    <div className="lecture-App">
-      <div className="lecture-App-main">
-        <div className="lecture-front">
-          <h1>Front End</h1>
-        </div>
-        <div className="lte">
-          <img src="https:ifh.cc/g/6AW38r.png" width="800px" />
-        </div>
-        <div className="lecture-mlyon">
-          <div className="lecture-varil">
-            <h2>여태까지 경험하지 못한 강의</h2>
-            <p>
-              새롭게 선보이는 it da의 강의 컨텐츠
-              <br />
-              이 컨텐츠를 가지고, 우리는 다시 한번 도약합니다.
-              <br />
-              품질 좋은 강의를 유지하기 위해 끊임 없이 노력하는
-              <br />
-              그야말로 it da의 최고의 콘텐츠 입니다.
-            </p>
+    <>
+      <Nav />
+      <div className="lecture-App">
+        <div className="lecture-App-main">
+          <div className="lecture-front">
+            <h1>Front End</h1>
           </div>
-        </div>
-        <div className="bg-secondary bg-opacity -25 pb-4">
-          <section className="container oberflow-hidden">
-            <div className="row g-5 mt-4">
-              <div className="col-sm-6 col-lg-3">
-                <div
-                  className="card"
-                  id="cursor"
-                  onClick={() => {
-                    navigate("/sorry");
-                  }}
-                >
-                  <div className="App-lecture">
-                    <div className="lecture">
-                      <h2 className="card-title fs-5">
-                        <Card products={products[0]} i={1} />
-                      </h2>
-                      <h2 className="card-title fs-5">
-                        <Card products={products[1]} i={2} />
-                      </h2>
-                      <h2 className="card-title fs-5">
-                        <Card products={products[2]} i={3} />
-                      </h2>
-                    </div>
-                    <div className="lecture">
-                      <h2 className="card-title fs-5">
-                        <Card products={products[3]} i={4} />
-                      </h2>
-                      <h2 className="card-title fs-5">
-                        <Card products={products[4]} i={5} />
-                      </h2>
-                      <h2 className="card-title fs-5">
-                        <Card products={products[5]} i={6} />
-                      </h2>
+          <div className="lte">
+            <img src="https:ifh.cc/g/6AW38r.png" width="800px" />
+          </div>
+          <div className="lecture-mlyon">
+            <div className="lecture-varil">
+              <h2>여태까지 경험하지 못한 강의</h2>
+              <p>
+                새롭게 선보이는 it da의 강의 컨텐츠
+                <br />
+                이 컨텐츠를 가지고, 우리는 다시 한번 도약합니다.
+                <br />
+                품질 좋은 강의를 유지하기 위해 끊임 없이 노력하는
+                <br />
+                그야말로 it da의 최고의 콘텐츠 입니다.
+              </p>
+            </div>
+          </div>
+          <div className="bg-secondary bg-opacity -25 pb-4">
+            <section className="container oberflow-hidden">
+              <div className="row g-5 mt-4">
+                <div className="col-sm-6 col-lg-3">
+                  <div
+                    className="card"
+                    id="cursor"
+                    onClick={() => {
+                      navigate("/sorry");
+                    }}
+                  >
+                    <div className="App-lecture">
+                      <div className="lecture">
+                        <h2 className="card-title fs-5">
+                          <Card products={products[0]} i={1} />
+                        </h2>
+                        <h2 className="card-title fs-5">
+                          <Card products={products[1]} i={2} />
+                        </h2>
+                        <h2 className="card-title fs-5">
+                          <Card products={products[2]} i={3} />
+                        </h2>
+                      </div>
+                      <div className="lecture">
+                        <h2 className="card-title fs-5">
+                          <Card products={products[3]} i={4} />
+                        </h2>
+                        <h2 className="card-title fs-5">
+                          <Card products={products[4]} i={5} />
+                        </h2>
+                        <h2 className="card-title fs-5">
+                          <Card products={products[5]} i={6} />
+                        </h2>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
         </div>
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
 
