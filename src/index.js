5e2d6a652163454c72c98652a89df64338b2c043
Joon Park <sargadil@gmail.com>
2023-05-08 10:25:53 +0900

    index

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
