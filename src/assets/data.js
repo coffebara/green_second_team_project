4a5cc160b272b2b51159c440aec5c5557b780eb0
FeliceKP <qpalzm192837@naver.com>
2023-05-09 16:26:59 +0900

    hs

--- src/assets/data.js
+++ src/assets/data.js
@@ -18,6 +18,7 @@ let data = [
     content:
       "실제 웹 UI를 여러개 만들어보며 배워보는 JavaScript 기초 수업입니다. 문법만 쭉 나열하면서 가르치면 재미없어서 강의 끄고 유튜브 보러갈 것이 뻔하니 실무 예제 중심의…",
     price: 78000,
+    price1: 165000,
     quantity: 1,
     image: "https://ifh.cc/g/ssAmtL.png",
     detailImage: "https://ifh.cc/g/TFcT2y.png",
@@ -30,6 +31,7 @@ let data = [
     content:
       " JavaScript ES6 신문법과 객체지향을 배울 수 있는 자바스크립트 심화과정입니다. (기초강의 아님 주의) 여러분이 어려워하던 this, class, prototype, 객체지향 개념과 더불어 spread, arrow, async, Promise,…",
     price: 56000,
+    price1: 120000,
     quantity: 1,
     image: "https://ifh.cc/g/Snlkd5.png",
     detailImage: "https://ifh.cc/g/vHhFSQ.jpg",
@@ -42,7 +44,8 @@ let data = [
     title: "React 리액트 기초부터 쇼핑몰 프로젝트까지!",
     content:
       "  리액트 쉽게 설명하기 장인, 코딩애플입니다. '상태관리를 위해 객체를 부모 컴포넌트의 state로부터 props로 받아와서 리턴해주세요' 같은 변태 개발자용어 쓰면서 리액트 어렵게 설명하는 나쁜 사람들이 많아서…",
-    price: 75000,
+    price: 83000,
+    price1: 170000,
     quantity: 1,
     image: "https://ifh.cc/g/MLthrr.png",
     detailImage: "https://ifh.cc/g/yjytct.jpg",
@@ -56,6 +59,7 @@ let data = [
     content:
       "프로그래밍 세계 최고로 쉽게 설명해드리는 코딩애플과 함께라면 웹서비스 제작도 뚝딱입니다. 게시판 하나 만들며 배워보는 Node.js 강의가 신설되었습니다!",
     price: 75000,
+    price1: 170000,
     quantity: 1,
     image: "https://ifh.cc/g/GzWvjM.png",
     detailImage: "https://ifh.cc/g/VWQaw1.png",
@@ -68,7 +72,8 @@ let data = [
     title: "Next.js로 웹서비스 만들기",
     content:
       "Next.js는 프론트엔드부터 서버까지 만들 수 있는 React기반 프레임워크입니다. 이것만 사용해도 풀스택 웹개발이 가능합니다. ",
-    price: 75000,
+    price: 35000,
+    price1: 70000,
     quantity: 1,
     image: "https://ifh.cc/g/krLoqY.png",
     detailImage: "https://ifh.cc/g/YqrSTT.png",
