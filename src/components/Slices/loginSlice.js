f7f10298deb6d27f87e8534a3ca0165b919ceea0
joohong-kim <zzangtra@gmail.com>
2023-05-09 16:56:23 +0900

    login logout slice

---
+++ src/components/Slices/loginSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let login = createSlice({
+  name: "login",
+  initialState: { isLogin: false },
+  reducers: {
+    loginToggle(state, action) {
+      state.isLogin = action.payload;
+      console.log("기본값: " + state.isLogin + " 변경 값: " + action.payload);
+    },
+    logoutToggle(state) {
+      state.isLogin = false;
+      console.log("로그아웃됨");
+    },
+  },
+});
+
+export const { loginToggle, logoutToggle } = login.actions;
+export default login.reducer;
