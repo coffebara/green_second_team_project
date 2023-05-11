2aea306e96e60dbffd9c84f73446711cc9015cb3
allready-dawn <qwshpke@naver.com>
2023-05-11 16:29:41 +0900

    카트 & 결제 페이지 네비 토글 업데이트

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,7 +2,7 @@ import "../test2/Test2.css";
 
 import ReactSwitch from "react-switch";
 import { Navbar, Container, Nav, Badge } from "react-bootstrap";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Cart from "../components/Cart";
@@ -11,13 +11,30 @@ import Footer from "../components/Footer";
 export const ThemeContext = createContext(null);
 
 export default function CartPage() {
-    let navigate = useNavigate();
+
 
     let state = useSelector((state) => state);
+
     const [theme, setTheme] = useState("light");
-    const toggleTheme = () => {
-        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    const setMode = (mode) => {
+        window.localStorage.setItem("theme", mode);
+        setTheme(mode);
     };
+
+    const toggleTheme = () => {
+        setTheme((themeMode) => {
+            const newTheme = themeMode === "light" ? "dark" : "light";
+            setMode(newTheme);
+            return newTheme;
+        });
+    };
+
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem("theme");
+        localTheme ? setTheme(localTheme) : setTheme("dark");
+    }, []);
+
+    let navigate = useNavigate();
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
@@ -25,14 +42,14 @@ export default function CartPage() {
                 <div className="Nav_Theme">
                     <Navbar>
                         <Container>
-                        <Nav.Link
-                                    onClick={() => {
-                                        navigate("/");
-                                    }}
-                                    className="Nav_Toggletheme"
-                                >
-                                    npm
-                                </Nav.Link>
+                            <Nav.Link
+                                onClick={() => {
+                                    navigate("/");
+                                }}
+                                className="Nav_Toggletheme"
+                            >
+                                npm
+                            </Nav.Link>
                             <Nav>
                                 <div className="Nav_Switch">
                                     <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} className="mt-2" />
@@ -68,7 +85,11 @@ export default function CartPage() {
                                     className="Nav_Toggletheme"
                                 >
                                     장바구니
-                                    {state.cart.length ? <Badge className='ms-2' bg="secondary">{state.cart.length}</Badge>:null}
+                                    {state.cart.length ? (
+                                        <Badge className="ms-2" bg="secondary">
+                                            {state.cart.length}
+                                        </Badge>
+                                    ) : null}
                                 </Nav.Link>
                             </Nav>
                         </Container>
