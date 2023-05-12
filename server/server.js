c2843238ddeaf891baad1a9f10a1a90769324098
joohong-kim <zzangtra@gmail.com>
2023-05-12 10:33:59 +0900

    dd

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const test = require('.//Router/test');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const test = require(".//Router/test");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(express.json());
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 
 const uri =
-  'mongodb+srv://admin:1q2w3e4r@joon.seyvdep.mongodb.net/?retryWrites=true&w=majority';
+  "mongodb+srv://admin:1q2w3e4r@joon.seyvdep.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,9 +23,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
+      "Pinged your deployment. You successfully connected to MongoDB!"
     );
     // const db = client.db('forum');
     // let result = await db.collection('comment').find().toArray();
@@ -37,31 +37,31 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.use('/', test);
+app.use("/", test);
 
-app.get('/comment', async (req, res) => {
+app.get("/comment", async (req, res) => {
   await client.connect();
-  const db = client.db('Team1');
+  const db = client.db("Team1");
   let comment = await db
-    .collection('comment')
+    .collection("comment")
     .find({ parents: req.query.url })
     .toArray();
-    console.log(req.query.url)
+  console.log(req.query.url);
   res.json(comment);
 });
 
-app.post('/commentwriting', async (req, res) => {
-  if (req.content == '') {
-    return res.status(500).json('빈 댓글은 작성이 불가합니다.');
+app.post("/commentwriting", async (req, res) => {
+  if (req.content == "") {
+    return res.status(500).json("빈 댓글은 작성이 불가합니다.");
   } else {
     await client.connect();
-    const db = client.db('Team1');
-    let comment = await db.collection('comment').insertOne(req.body);
+    const db = client.db("Team1");
+    let comment = await db.collection("comment").insertOne(req.body);
     res.json(comment);
   }
 });
 
-const port = process.env.PORT || 5000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
+const port = process.env.PORT || 5001; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- src/components/Reperence/Comment.js
+++ src/components/Reperence/Comment.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import '../../styles/reference.css';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import "../../styles/reference.css";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 // firebase
 // import { AuthContext } from "../context/AuthContext";
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Comment() {
   // 댓글 텍스트용
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const handleChange = (e) => {
     setText(e.target.value);
   };
@@ -17,20 +17,19 @@ export default function Comment() {
   // 현재 날짜 생성
   let nowDate = new Date();
   let koreaTime = new Date(
-    nowDate.toLocaleString('en-US', { timeZone: 'Asia/Seoul' })
+    nowDate.toLocaleString("en-US", { timeZone: "Asia/Seoul" })
   );
 
   // 서버용
   let state = useSelector((state) => state);
   const [comment, setComment] = useState([]);
   const [data, setData] = useState([]);
-  
 
   useEffect(() => {
     axios
-    .get('http://localhost:5000/comment?url='+window.location.href)
-    .then((res) => setComment(res.data))
-    .catch((err) => console.log(err));
+      .get("http://localhost:5001/comment?url=" + window.location.href)
+      .then((res) => setComment(res.data))
+      .catch((err) => console.log(err));
   });
 
   const comment_btn = () => {
@@ -38,11 +37,11 @@ export default function Comment() {
       console.log(window.location.href);
     } else {
       axios
-        .post('http://localhost:5000/commentwriting', {
+        .post("http://localhost:5001/commentwriting", {
           name: name,
           parents: window.location.href,
           date: koreaTime,
-          good: 'good',
+          good: "good",
           content: text,
         })
         .then((response) => {
@@ -51,20 +50,20 @@ export default function Comment() {
         .catch((error) => {
           console.log(error);
         });
-      setText('');
+      setText("");
     }
   };
   //플레이스홀더
-  const placeholderText = state.login.isLogin ? '' : '로그인 후 이용해주세요.';
+  const placeholderText = state.login.isLogin ? "" : "로그인 후 이용해주세요.";
 
   // 파이어베이스에서 로그인한 계정의 이름을 가져옴
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const auth = getAuth();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setName(user.displayName);
-        placeholderText = '';
+        placeholderText = "";
       }
     });
     return () => {
